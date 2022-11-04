6aa86f3cf571a16740d7d038d61967add8f5fe7f
gandergo <emailou@gmail.com>
2022-11-04 09:39:15 +0800

    fix: Merge Update create_blend_file_from_session_data.py

--- src/blender_stuff/create_blend_file_from_session_data.py
+++ src/blender_stuff/create_blend_file_from_session_data.py
@@ -30,6 +30,7 @@ def create_blend_file_from_session_data(
     # )
 
     try:
+        blender_exe_path = "/Applications/Blender.app/Contents/MacOS/Blender"
         blender_exe_path = Path(blender_exe_path)
         if not blender_exe_path.exists():
             raise FileNotFoundError(
@@ -52,7 +53,7 @@ def create_blend_file_from_session_data(
     logger.info(f"Starting `blender` sub-process with this command: \n {command_list}")
 
     blender_process = subprocess.Popen(
-        command_list, shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+        command_list, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE
     )
     while True:
         output = blender_process.stdout.readline()
